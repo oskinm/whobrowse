76f7865f259e250dafb5bcf14bf0dfbefa1dfd17
Mikhail Oskin <oskinm@live.ru>
2018-11-29 23:34:17 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='What information can we get from IIS log?',
+    author='Mikhail Oskin',
+    license='MIT',
+)
